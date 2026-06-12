37d9d5cf52a032497764d0d44e2577afededed7a
Vinay Butty <vinay.butty@tcpwave.com>
2026-06-12 12:31:36 +0530

    Add NVD CVE collection functionality

--- collectors/cve_collector.py
+++ collectors/cve_collector.py
@@ -4,7 +4,13 @@ URL = "https://services.nvd.nist.gov/rest/json/cves/2.0?resultsPerPage=5"
 
 response = requests.get(URL, timeout=30)
 
-if response.status_code == 200:
-    print("Connected to NVD API")
-else:
+if response.status_code != 200:
     print(f"Failed: {response.status_code}")
+    exit(1)
+
+data = response.json()
+
+print(f"Total CVEs Returned: {len(data['vulnerabilities'])}")
+
+for cve in data["vulnerabilities"]:
+    print(cve["cve"]["id"])
